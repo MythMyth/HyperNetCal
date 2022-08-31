e4665382def6d2ec0454d3e63d434acef8f5afea
MythMyth <windy10091996@gmail.com>
2022-08-31 23:48:52 +0700

    App

--- HyperNetCal/Form1.Designer.cs
+++ HyperNetCal/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tickets = new System.Windows.Forms.TextBox();
+            this.Ranking = new System.Windows.Forms.ListBox();
+            this.cal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tickets
+            // 
+            this.tickets.Location = new System.Drawing.Point(12, 12);
+            this.tickets.Multiline = true;
+            this.tickets.Name = "tickets";
+            this.tickets.Size = new System.Drawing.Size(289, 402);
+            this.tickets.TabIndex = 0;
+            // 
+            // Ranking
+            // 
+            this.Ranking.FormattingEnabled = true;
+            this.Ranking.ItemHeight = 15;
+            this.Ranking.Location = new System.Drawing.Point(470, 12);
+            this.Ranking.Name = "Ranking";
+            this.Ranking.Size = new System.Drawing.Size(318, 394);
+            this.Ranking.TabIndex = 1;
+            // 
+            // cal
+            // 
+            this.cal.Location = new System.Drawing.Point(344, 198);
+            this.cal.Name = "cal";
+            this.cal.Size = new System.Drawing.Size(75, 23);
+            this.cal.TabIndex = 2;
+            this.cal.Text = "Cal";
+            this.cal.UseVisualStyleBackColor = true;
+            this.cal.Click += new System.EventHandler(this.cal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.cal);
+            this.Controls.Add(this.Ranking);
+            this.Controls.Add(this.tickets);
+            this.Name = "Form1";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox tickets;
+        private ListBox Ranking;
+        private Button cal;
     }
 }
--- HyperNetCal/Form1.cs
+++ HyperNetCal/Form1.cs
@@ -6,5 +6,38 @@ namespace HyperNetCal
         {
             InitializeComponent();
         }
+
+        private void cal_Click(object sender, EventArgs e)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            string[] t = tickets.Text.ToString().Split('\n');
+            foreach(string ticket in t)
+            {
+                string s = ticket.Trim().ToLower();
+                foreach(char c in s)
+                {
+                    if(map.ContainsKey(c)) { map[c] = map[c] + 1; }
+                    else { map.Add(c, 1); }
+                }
+            }
+
+            List<(string, int)> values = new List<(string, int)> ();
+            foreach(string ticket in t)
+            {
+                string s = ticket.Trim().ToLower();
+                int point = 0;
+                foreach(char c in s)
+                {
+                    point += map[c];
+                }
+                values.Add((ticket.ToUpper(), point));
+            }
+            values = values.OrderBy(o => o.Item2).ToList();
+            Ranking.Items.Clear();
+            foreach((string, int) item in values)
+            {
+                Ranking.Items.Add($"{item.Item1} - {item.Item2}");
+            }
+        }
     }
 }
